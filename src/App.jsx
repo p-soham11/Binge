60ea5196da198be975ba6990bcf9fafe64b96fc4
p-soham11 <sohampal1108@gmail.com.com>
2023-11-12 07:30:22 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+import {fetchDataFromApi} from "./utils/api";
+
+function App() {
+
+  useEffect(() => {
+    apiTest
+  }, [])
+
+  const apiTest = () => {
+    fetchDataFromApi('/movie/popular')
+      .then((res) => {
+        console.log(res)
+      })
+  }
+
+  return (
+    <div className="App">
+      App
+    </div>
+  )
+}
+
+export default App
