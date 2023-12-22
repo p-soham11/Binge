f01b98e97625cf045df8e6fba7729be9ae6c4104
p-soham11 <sohampal1108@gmail.com.com>
2023-12-22 18:37:50 +0530

    Updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { store } from './store/store.js'
-import { Provider } from 'react-redux'
-import App from './App.jsx'
-import './index.scss'
+/**
+ * eslint-disable no-unused-vars
+ *
+ * @format
+ */
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { store } from "./store/store.js";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import "./index.scss";
+
+//  main.jsx is our top level component.
+//  we pass the Redux store to give every component the access to the Redux variables
+ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
         <App />
     </Provider>
-)
+);
